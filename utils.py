ce74231a47dd79d201b3f546a2015cbf663282f1
Hunter Barton <hbarton1337@gmail.com>
2024-06-01 12:06:11 -0700

    Mess with data added

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import librosa
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 #Show given an mp3 filepath
 def show_spectrogram_filepath(file_path):
@@ -27,8 +28,39 @@ def show_spectrogram(S_dB):
     fig.colorbar(img, ax=ax, format="%+2.0f dB")
 
 
+#Show the difference between 2 mp3s.
+#They might have different max and min values so I normalize after getting the difference
+#Normalizing is done by taking the difference and dividing by the range of the values between the two arrays to get a percentage difference
 def diff_mp3(array, other):
-    total_diff = np.sum(np.abs(array - other))
+
+    popMax = max(np.max(array), np.max(other))
+    popMin = min(np.min(array), np.min(other))
+
+    total_diff = np.abs(array - other)
+
+    total_diff = total_diff / (popMax - popMin)
 
     print("Average:", np.average(total_diff))
     print("Stdev:", np.std(total_diff))
+    print("Max:", np.max(total_diff))
+    print("Min:", np.min(total_diff))
+
+
+#Meant to be used with dB values
+#Mess percent is positive and negative direction, so 10% would be +- 10%
+def mess_values(input, mess_percent):
+    fmax = np.max(input)
+    fmin = np.min(input)
+
+    new_arr = np.copy(input)
+
+    range = fmax - fmin
+
+    diff = range * mess_percent / 50
+
+    for y in new_arr:
+        for x in y:
+            x += (random.uniform(0, 1) * diff) - (diff / 2)
+
+    return new_arr
+
